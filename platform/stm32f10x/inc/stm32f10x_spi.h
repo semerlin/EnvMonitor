f07efd3e8d7959f475abded4acdcc1c0d5c1b9bc
Huang Yang <hyjstz@163.com>
2016-12-28 23:58:32 +0800

    add stm32f10x spi module

---
+++ platform/stm32f10x/inc/stm32f10x_spi.h
@@ -0,0 +1,142 @@
+#ifndef _STM32F10X_SPI_H_
+  #define _STM32F10X_SPI_H_
+
+#include "sysdef.h"
+
+/* spi group definition */
+typedef enum
+{
+    SPI1,
+    SPI2,
+    SPI3,
+    SPI_Count,
+}SPI_Group;
+
+/* main mode */
+#define SPI_MainMode_Slave          (0x00)
+#define SPI_MainMode_Master         (1 << 2)
+#define IS_SPI_MainMode_Param(mode) ((mode == SPI_MainMode_Slave) || \
+                                     (mode == SPI_MainMode_Master))
+
+/* sub mode */
+#define SPI_SubMode_Full_Duplex                (0x00)
+#define SPI_SubMode_BiDirection_Write          (3 << 14)
+#define SPI_SubMode_BiDIrection_Recv           (1 << 15)
+#define SPI_SubMode_UniDirection_RecvOnly      (1 << 10)
+#define IS_SPI_SubMode_Param(mode) ((mode == SPI_SubMode_Full_Duplex) || \
+                                    (mode == SPI_SubMode_BiDirection_Write) || \
+                                    (mode == SPI_SubMode_BiDIrection_Recv) || \
+                                    (mode == SPI_SubMode_UniDirection_RecvOnly))
+
+
+/* clock division */
+#define SPI_Clk_Divided_2               (0x00)
+#define SPI_Clk_Divided_4               (1 << 3)
+#define SPI_Clk_Divided_8               (2 << 3)
+#define SPI_Clk_Divided_16              (3 << 3)
+#define SPI_Clk_Divided_32              (4 << 3)
+#define SPI_Clk_Divided_64              (5 << 3)
+#define SPI_Clk_Divided_128             (6 << 3)
+#define SPI_Clk_Divided_256             (7 << 3)
+#define IS_SPI_Clk_Param(clk)  ((clk == SPI_Clk_Divided_2) || \
+                                (clk == SPI_Clk_Divided_4) || \
+                                (clk == SPI_Clk_Divided_8) || \
+                                (clk == SPI_Clk_Divided_16) || \
+                                (clk == SPI_Clk_Divided_32) || \
+                                (clk == SPI_Clk_Divided_64) || \
+                                (clk == SPI_Clk_Divided_128) || \
+                                (clk == SPI_Clk_Divided_256))
+
+/* clock polarity */
+#define SPI_Polarity_low           (0x00)
+#define SPI_Polarity_High          (1 << 1)
+#define IS_SPI_Polority_Param(polarity)   ((polarity == SPI_Polarity_low) || \
+                                           (polarity == SPI_Polarity_High))
+
+/* clock phase */
+#define SPI_Phase_FirstClk           (0x00)
+#define SPI_Phase_SecondClk          (0x01)
+#define IS_SPI_Phase_Param(phase)   ((phase == SPI_Phase_FirstClk) || \
+                                     (phase == SPI_Phase_SecondClk))
+
+/* data length */
+#define SPI_DataLength_8Bits       (0x00)
+#define SPI_DataLength_16Bits      (1 << 11)
+#define IS_SPI_DataLength_Param(length)   \
+                        ((length == SPI_DataLength_8Bits) || \
+                         (length == SPI_DataLength_16Bits))
+
+
+/* data format */
+#define SPI_DataFormat_MSB   (0x00)
+#define SPI_DataFormat_LSB   (1 << 7)                    
+#define IS_SPI_DataFormat_Param(format)   \
+                        ((format == SPI_DataFormat_MSB) || \
+                         (format == SPI_DataFormat_LSB))
+
+
+/* nss mode*/
+#define SPI_NSS_HARDWARE  (0x00)
+#define SPI_NSS_SOFTWARE  (1 << 9)  
+#define IS_SPI_NSS_Param(NSS) ((NSS == SPI_NSS_HARDWARE) || \
+                               (NSS == SPI_NSS_SOFTWARE))
+
+/* interrupt */
+#define SPI_IT_TXE     (1 << 7)
+#define SPI_IT_RXNE    (1 << 6)
+#define SPI_IT_ERRIE   (1 << 5)
+#define IS_SPI_IT_Param(IT) ((IT == SPI_IT_TXE) || \
+                             (IT == SPI_IT_RXNE) || \
+                             (IT == SPI_IT_ERRIE))
+
+/* flag */
+#define SPI_Flag_BSY           (1 << 7)
+#define SPI_Flag_OVR           (1 << 6)
+#define SPI_Flag_MODF          (1 << 5)
+#define SPI_Flag_CRCERR        (1 << 4)
+#define SPI_Flag_UDR           (1 << 3)
+#define SPI_Flag_CHSIDE        (1 << 2)
+#define SPI_Flag_TXE           (1 << 1)
+#define SPI_Flag_RXNE          (1 << 0)
+#define IS_SPI_Flag_Param(flag) ((flag == SPI_Flag_BSY) || \
+                                 (flag == SPI_Flag_OVR) || \
+                                 (flag == SPI_Flag_MODF) || \
+                                 (flag == SPI_Flag_CRCERR) || \
+                                 (flag == SPI_Flag_UDR) || \
+                                 (flag == SPI_Flag_CHSIDE) || \
+                                 (flag == SPI_Flag_TXE) || \
+                                 (flag == SPI_Flag_RXNE))
+
+/* spi config structure */
+typedef struct
+{
+    uint32 mainMode;
+    uint32 subMode;
+    uint32 clock;
+    uint32 polarity;
+    uint32 phase;
+    uint32 length;
+    uint32 format;
+    uint32 nssMode;
+    BOOL useCrc;
+}SPI_Config;
+
+
+/* interface */
+void SPI_Enable(__in SPI_Group group, __in BOOL flag);
+void SPI_Setup(__in SPI_Group group, __in const SPI_Config *config);
+void SPI_StructInit(__inout SPI_Config *config);
+void SPI_EnableInt(__in SPI_Group group, __in uint8 intflag, __in BOOL flag);
+BOOL SPI_IsFlagOn(__in SPI_Group group, __in uint8 flag);
+void SPI_SetCRCNext(__in SPI_Group group, __in BOOL flag);
+void SPI_EnableTxDMA(__in SPI_Group group, __in BOOL flag);
+void SPI_EnableRxDMA(__in SPI_Group group, __in BOOL flag);
+void SPI_WriteData(__in SPI_Group group, __in uint16 data);
+uint16 SPI_ReadData(__in SPI_Group group);
+void SPI_SetCRCPolynomial(__in SPI_Group group, __in uint16 data);
+uint16 SPI_GetRxCRC(__in SPI_Group group);
+uint16 SPI_GetTxCRC(__in SPI_Group group);
+
+
+#endif
+
---
+++ platform/stm32f10x/src/stm32f10x_spi.c
@@ -0,0 +1,345 @@
+#include "stm32f10x_spi.h"
+#include "stm32f10x_map.h"
+#include "stm32f10x_cfg.h"
+#include "stm32f10x_rcc.h"
+
+/* spi structure */
+typedef struct 
+{
+    volatile uint16 CR1;
+    uint16 RESERVED0;
+    volatile uint16 CR2;
+    uint16 RESERVED1;
+    volatile uint16 SR;
+    uint16 RESERVED2;
+    volatile uint16 DR;
+    uint16 RESERVED3;
+    volatile uint16 CRCPR;
+    uint16 RESERVED4;
+    volatile uint16 RXCRCR;
+    uint16 RESERVED5;
+    volatile uint16 TXCRCR;
+    uint16 RESERVED6;
+    volatile uint16 I2SCFGR;
+    uint16 RESERVED7;
+    volatile uint16 I2SPR;
+    uint16 RESERVED8;
+}SPI_T;
+
+/* SPI group array */
+static SPI_T * const SPIx[] = {(SPI_T *)SPI1_BASE, 
+                               (SPI_T *)SPI2_BASE,
+                               (SPI_T *)SPI3_BASE};
+
+
+/* SPI register bit definition */
+#define CR1_MSTR   (1 << 2)
+#define CR1_BR     (0x07 << 3)
+#define CR1_BIDIMODE  (1 << 15)
+#define CR1_BIDIOE (1 << 14)
+#define CR1_CRCEN (1 << 13)
+#define CR1_CRCNEXT (1 << 12)
+#define CR1_DFF (1 << 11)
+#define CR1_RXONLY (1 << 10)
+#define CR1_SSM (1 << 9)
+#define CR1_SSI (1 << 8)
+#define CR1_LSBFIRST (1 << 7)
+#define CR1_SPE (1 << 6)
+#define CR1_CPOL  (1 << 1)
+#define CR1_CPHA (0x01)
+
+#define CR2_SSOE (1 << 2)
+#define CR2_TXDMAEN (1 << 1)
+#define CR2_RXDMAEN (0x01)
+
+#define SR_RXNE   (0x01)
+#define SR_TXE    (1 << 1)
+#define SR_BSY    (1 << 7)
+
+/**
+ * @brief enable or disable spi
+ * @param spt group
+ * @param enable or disable flag
+ */
+void SPI_Enable(__in SPI_Group group, __in BOOL flag)
+{
+    assert_param(group < SPI_Count);
+    SPI_T * const SpiX = SPIx[group];
+    
+    if(flag)
+        SpiX->CR1 |= CR1_SPE;
+    else
+    {
+        if(SpiX->CR1 & CR1_SPE)
+        {
+            if(((SpiX->CR1 & CR1_BIDIMODE) == 0) &&
+               ((SpiX->CR1 & CR1_RXONLY) == 0))
+            {
+                while(SpiX->SR & SR_BSY);
+            }
+            else if(((SpiX->CR1 & CR1_BIDIMODE) == 1) &&
+                    ((SpiX->CR1 & CR1_BIDIOE) == 1))
+            {
+                while(SpiX->SR & SR_BSY);
+            }
+            
+            SpiX->CR1 &= ~CR1_SPE;
+        }
+    }
+}
+
+/**
+ * @brief setup spi
+ * @param spi group
+ * @param configure parameters
+ */
+void SPI_Setup(__in SPI_Group group, __in const SPI_Config *config)
+{
+    assert_param(group < SPI_Count);
+    assert_param(IS_SPI_MainMode_Param(config->mainMode));
+    assert_param(IS_SPI_SubMode_Param(config->subMode));
+    assert_param(IS_SPI_Clk_Param(config->clock));
+    assert_param(IS_SPI_Polority_Param(config->polarity));
+    assert_param(IS_SPI_Phase_Param(config->phase));
+    assert_param(IS_SPI_DataLength_Param(config->length));
+    assert_param(IS_SPI_DataFormat_Param(config->format));
+    assert_param(IS_SPI_NSS_Param(config->nssMode));
+    
+    SPI_T * const SpiX = SPIx[group];
+    
+    //set spi master or slave mode
+    SpiX->CR1 &= ~CR1_MSTR;
+    SpiX->CR1 |= config->mainMode;
+    
+    //set detail mode
+    SpiX->CR1 &= ~(CR1_BIDIMODE | CR1_BIDIOE | CR1_RXONLY);
+    SpiX->CR1 |= config->subMode;
+    
+    //set clock
+    uint32 pclk = RCC_GetPCLK2();
+    switch(group)
+    {
+    case SPI1:
+        pclk = RCC_GetPCLK2();
+        break;
+    case SPI2:
+    case SPI3:
+        pclk = RCC_GetPCLK1();
+        break;
+    default:
+        break;
+    }
+    SpiX->CR1 &= ~CR1_BR;
+    SpiX->CR1 |= config->clock;
+    
+    //set polarity
+    SpiX->CR1 &= ~CR1_CPOL;
+    SpiX->CR1 |= config->polarity;
+    
+    //set phase
+    SpiX->CR1 &= ~CR1_CPHA;
+    SpiX->CR1 |= config->phase;
+
+    //set data length
+    SpiX->CR1 &= ~CR1_DFF;
+    SpiX->CR1 |= config->length;
+    
+    //set frame format
+    SpiX->CR1 &= ~CR1_LSBFIRST;
+    SpiX->CR1 |= config->format;
+    
+    //set nss mode
+    if(config->nssMode == SPI_NSS_SOFTWARE)
+    {
+        SpiX->CR1 |= CR1_SSM;
+        if(config->mainMode == SPI_MainMode_Master)
+            SpiX->CR1 |= CR1_SSI;
+        else
+            SpiX->CR1 &= ~CR1_SSI;
+    }
+    else
+    {
+        SpiX->CR1 &= ~CR1_SSM;
+        if(config->mainMode == SPI_MainMode_Master)
+            SpiX->CR2 |= CR2_SSOE;
+        else
+            SpiX->CR2 &= ~CR2_SSOE;
+    }
+    
+    //set crc
+    if(config->useCrc)
+        SpiX->CR1 |= CR1_CRCEN;
+    else
+        SpiX->CR1 &= ~CR1_CRCEN;
+    
+    
+}
+
+/**
+ * @brief init spi config structure 
+ * @param spi config structure
+ */
+void SPI_StructInit(__inout SPI_Config *config)
+{
+    assert_param(config != NULL);
+    config->mainMode = SPI_MainMode_Master;
+    config->subMode = SPI_SubMode_Full_Duplex;
+    config->clock = SPI_Clk_Divided_2;
+    config->polarity = SPI_Polarity_High;
+    config->phase = SPI_Phase_FirstClk;
+    config->format = SPI_DataFormat_MSB;
+    config->length = SPI_DataLength_8Bits;
+    config->nssMode = SPI_NSS_SOFTWARE;
+    config->useCrc = FALSE;
+}
+
+/**
+ * @brief enable or disable spi interrupt
+ * @param spi group
+ * @param interrupt name
+ * @param enable or disable flag
+ */
+void SPI_EnableInt(__in SPI_Group group, __in uint8 intFlag, __in BOOL flag)
+{
+    assert_param(group < SPI_Count);
+    assert_param(IS_SPI_IT_Param(intFlag));
+    
+    SPI_T * const SpiX = SPIx[group];
+    if(flag)
+        SpiX->CR2 |= intFlag;
+    else
+        SpiX->CR2 &= ~intFlag;
+    
+}
+
+/**
+ * @brief check if specified flag is on 
+ * @param spi group
+ * @param flag need to check
+ * @return flag status
+ */
+BOOL SPI_IsFlagOn(__in SPI_Group group, __in uint8 flag)
+{   
+    assert_param(group < SPI_Count);
+    assert_param(IS_SPI_Flag_Param(flag));
+    
+    SPI_T * const SpiX = SPIx[group];
+    
+    if((SpiX->SR & flag) != 0)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+/**
+ * @brief set next character is crc 
+ * @param spi group
+ * @param next character is crr or not 
+ */
+void SPI_SetCRCNext(__in SPI_Group group,__in BOOL flag)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    if(flag)
+        SpiX->CR1 |= CR1_CRCNEXT;
+    else
+        SpiX->CR1 &= ~CR1_CRCNEXT;
+}
+
+/**
+ * @brief enable tx transfer use dma or not 
+ * @param spi group
+ * @param enable or not
+ */
+void SPI_EnableTxDMA(__in SPI_Group group, __in BOOL flag)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    if(flag)
+        SpiX->CR2 |= CR2_TXDMAEN;
+    else
+        SpiX->CR2 &= ~CR2_TXDMAEN;
+}
+
+/**
+ * @brief enable rx transfer use dma or not 
+ * @param spi group
+ * @param enable or not
+ */
+void SPI_EnableRxDMA(__in SPI_Group group, __in BOOL flag)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    if(flag)
+        SpiX->CR2 |= CR2_RXDMAEN;
+    else
+        SpiX->CR2 &= ~CR2_RXDMAEN;
+}
+
+/**
+ * @brief write data to spi port 
+ * @param spi group
+ * @param data to write
+ */
+void SPI_WriteData(__in SPI_Group group, __in uint16 data)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    SpiX->DR = data;
+}
+
+/**
+ * @brief read data from spi port 
+ * @param spi group
+ * @return data return 
+ */
+uint16 SPI_ReadData(__in SPI_Group group)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    return SpiX->DR;
+}
+
+/**
+ * @brief set crc polynomial 
+ * @param spi group
+ * @param crc polynomial
+ */
+void SPI_SetCRCPolynomial(__in SPI_Group group, __in uint16 data)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    SpiX->CRCPR = data;
+}
+
+/**
+ * @brief  get rx crc value
+ * @param spi group
+ * @return rx crc value
+ */
+uint16 SPI_GetRxCRC(__in SPI_Group group)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    return SpiX->RXCRCR;
+}
+
+/**
+ * @brief get tx crc value
+ * @param spi group
+ * @return tx crc value
+ */
+uint16 SPI_GetTxCRC(__in SPI_Group group)
+{
+    assert_param(group < SPI_Count);
+    
+    SPI_T * const SpiX = SPIx[group];
+    return SpiX->RXCRCR;
+}
