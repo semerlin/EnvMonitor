f1b877ca2740d67ded23b800d3be460fac53a541
Huang Yang <hyjstz@163.com>
2017-01-14 13:54:35 +0800

    enable check stack overflow

--- board/FreeRTOSConfig.h
+++ board/FreeRTOSConfig.h
@@ -26,13 +26,14 @@
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet		0
-#define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete				1
-#define INCLUDE_vTaskCleanUpResources	0
-#define INCLUDE_vTaskSuspend			0
-#define INCLUDE_vTaskDelayUntil			0
-#define INCLUDE_vTaskDelay				1
+#define INCLUDE_vTaskPrioritySet		        0
+#define INCLUDE_uxTaskPriorityGet		        0
+#define INCLUDE_vTaskDelete				        1
+#define INCLUDE_vTaskCleanUpResources	        0
+#define INCLUDE_vTaskSuspend			        0
+#define INCLUDE_vTaskDelayUntil			        0
+#define INCLUDE_vTaskDelay				        1
+#define INCLUDE_uxTaskGetStackHighWaterMark     1
 
 /* value can be 0(highest) to 15(lowest)*/
 #define configKERNEL_INTERRUPT_PRIORITY 		(15)
